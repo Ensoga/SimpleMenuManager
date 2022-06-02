b2bb6bc6a2f810c9dee4f7ed52d99670e39929bd
Ensoga <ensoga@gmail.com>
2022-06-02 23:38:36 +0200

    Initial commit

---
+++ Assets/MenuManagment/MenuToggle.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuToggle : MonoBehaviour
+{
+    MenuManager menuManager;
+    GameObject MenuScripts;
+    [HideInInspector] public bool currentMenu = false;
+
+    private void Awake()
+    {
+        MenuScripts = GameObject.Find("MenuScripts");
+        menuManager = MenuScripts.GetComponent<MenuManager>();
+    }
+
+    public void ButtonPressed()
+    {
+        currentMenu = !currentMenu;
+        if (currentMenu)
+        {
+            menuManager.DeactivateOtherMenu();
+        }
+        menuManager.ManageMenus();
+    }
+}
